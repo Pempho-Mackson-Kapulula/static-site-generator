822ba834bf712b2b77639a54e46942330e80d55c
Pempho Mackson Kapulula <pemphomacksonkapulula@gmail.com>
2026-04-08 17:18:22 +0200

    B: Initial project setup with main.py, main.sh, .gitignore, TextNode class, TextType enum, and basic printing

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import *
+
+def main():
+    node = TextNode("Testing text", TextType.TEXT,"https://www.google.com")
+
+    print(node)
+
+
+
+
+if __name__ == "__main__":
+    main()
